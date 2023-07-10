604732512b448de8c408b908154024a88a70b6c3
Gleb Mazovetskiy <glex.spb@gmail.com>
2023-07-10 05:10:50 +0100

    Re-encode CL2
    Original Blizzard encoder is slightly less optimal than our encoder.
    
    Savings for unpacked and minified MPQs:
    * diabdat.mpq: 918,311 bytes.
    * hellfire.mpq: 313,882 bytes.
    
    Example player graphics (note that only a few are loaded at any given time for single player):
    * diabdat/plrgfx/warrior/: 366,564 bytes.
    
    Example monster graphics:
    
    * diabdat/monsters/skelbow: 5,391 bytes.
    
    Fixes #5

--- src/public/include/cl22clx.hpp
+++ src/public/include/cl22clx.hpp
@@ -12,7 +12,10 @@
 namespace dvl_gfx {
 
 /**
- * @brief Converts a CL2 image to CLX in-place.
+ * @brief Converts a CL2 image to CLX.
+ *
+ * Re-encodes the frames. This can reduce file size because the dvl_gfx encoder
+ * is more optimal than the original encoder used by Blizzard.
  *
  * @param data The CL2 buffer.
  * @param size CL2 buffer size.
@@ -20,16 +23,30 @@ namespace dvl_gfx {
  * @param numWidths The number of widths.
  * @return std::optional<IoError>
  */
-std::optional<IoError> Cl2ToClx(uint8_t *data, size_t size,
+std::optional<IoError> Cl2ToClx(const uint8_t *data, size_t size,
+    const uint16_t *widths, size_t numWidths, std::vector<uint8_t> &out);
+
+/**
+ * @brief Converts a CL2 image to CLX in-place without re-encoding.
+ *
+ * Does not re-encode the frames.
+ *
+ * @param data The CL2 buffer.
+ * @param size CL2 buffer size.
+ * @param widths Widths of each frame. If all the frame are the same width, this can be a single number.
+ * @param numWidths The number of widths.
+ * @return std::optional<IoError>
+ */
+std::optional<IoError> Cl2ToClxNoReencode(uint8_t *data, size_t size,
     const uint16_t *widths, size_t numWidths);
 
 std::optional<IoError> Cl2ToClx(const char *inputPath, const char *outputPath,
-    const uint16_t *widths, size_t numWidths);
+    const uint16_t *widths, size_t numWidths, bool reencode = true);
 
 inline std::optional<IoError> Cl2ToClx(const char *inputPath, const char *outputPath,
-    const std::vector<uint16_t> &widths)
+    const std::vector<uint16_t> &widths, bool reencode = true)
 {
-	return Cl2ToClx(inputPath, outputPath, widths.data(), widths.size());
+	return Cl2ToClx(inputPath, outputPath, widths.data(), widths.size(), reencode);
 }
 
 /**
@@ -38,11 +55,12 @@ inline std::optional<IoError> Cl2ToClx(const char *inputPath, const char *output
  * @param inputPaths Paths to the input files.
  * @param numFiles The number of `inputPaths`.
  * @param widths Widths of each frame. If all the frame are the same width, this can be a single number.
+ * @param reencode If true, reencodes the CL2 graphics data (our encoder produces slightly smaller files).
  * @return std::optional<IoError>
  */
 std::optional<IoError> CombineCl2AsClxSheet(
     const char *const *inputPaths, size_t numFiles, const char *outputPath,
-    const std::vector<uint16_t> &widths);
+    const std::vector<uint16_t> &widths, bool reencode = true);
 
 } // namespace dvl_gfx
 #endif // DVL_GFX_CL22CLX_H_
